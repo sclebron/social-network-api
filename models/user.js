6729e0a52ec346e6c13f912e377cfea3c03abd94
SLeBron <sophieclebron@gmail.com>
2022-09-30 16:04:34 -0600

    changed file names

---
+++ models/user.js
@@ -0,0 +1,11 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: String, 
+    }
+);
+
+const User = model('user', userSchema);
+
+module.exports = User;
