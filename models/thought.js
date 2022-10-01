8256b64ba7b5c68cbb34cd79c7c20df5354b7945
SLeBron <sophieclebron@gmail.com>
2022-10-01 09:24:37 -0600

    updated thought model

--- models/thought.js
+++ models/thought.js
@@ -7,16 +7,27 @@ const reactionSchema = new Schema(
             default: () => new Types.ObjectId()
         },
         reactionBody: {
-            
+            type: String,
+            required: true,
+            maxLength: 280,
         },
         username: {
-
+            type: String,
+            required: true,
         },
         createdAt: {
-
-        }
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal),
+        },
+        {
+            toJSON: {
+                getters: true,
+            },
+            id: false,
+        },
     }
-)
+);
 
 const thoughtSchema = new Schema(
     {
