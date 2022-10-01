d83cd8f08a90ef2b71c099a0379174b16806d3c2
SLeBron <sophieclebron@gmail.com>
2022-09-30 22:56:00 -0600

    started thought model;

---
+++ models/thought.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require('mongoose');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal),
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        }
+    }
+);
+
