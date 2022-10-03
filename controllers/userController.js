028b4b0f6071a69d69c75c933a2124daba3d65bb
SLeBron <sophieclebron@gmail.com>
2022-10-02 21:51:38 -0600

    completed user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,9 +7,17 @@ module.exports = {
         .then((users) => res.json(users))
         .catch((err) => res.status(500).json(err));
     },
-    //get a single user by its id, to do - populate thought and friend data
+    //get a single user by its id
     getUserById(req, res) {
         User.findONe({_id: req.params.userId})
+            .populate({
+                path: 'thoughts',
+                select: '-__v'
+            })
+            .populate({
+                path: 'friends',
+                select: '-__v'
+            })
             .select('-__v')
             .then((user) => 
                 !user
@@ -24,5 +32,16 @@ module.exports = {
             .then((dbUserData) => res.json(dbUserData))
             .catch((err) => res.status(500).json(err));
     },
-    //to do - put to update a user by its id and delete to remove a user by its id
+    //update a user by its id
+    updateUser(req, res) {
+            User.findOneAndUpdate({_id: req.params.userId})
+            .then((dbUserData) => res.json(dbUserData))
+            .catch((err) => res.status(500).json(err));
+    },
+    //delete a user by its id
+    deleteUser(req, res) {
+            User.findOneAndDelete({_id: req.params.userId})
+            .then((dbUserData) => res.json(dbUserData))
+            .catch((err) => res.status(500).json(err));
+    },
 };
