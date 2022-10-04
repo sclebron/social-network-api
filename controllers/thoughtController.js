98766a459fd50a4c8b2a0d3e6abbc76eb727383a
SLeBron <sophieclebron@gmail.com>
2022-10-04 17:37:27 -0600

    fixed spelling error in thought controller

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -7,7 +7,7 @@ module.exports = {
         .then((dbUserData) => res.json(dbUserData))
         .catch((err) => res.status(500).json(err));
     },
-    getThoughtbyId(req, res) {
+    getThoughtById(req, res) {
         Thought.findOne({_id: req.params.thoughtId})
         .then((dbThoughtData) => 
                 !(dbThoughtData)
